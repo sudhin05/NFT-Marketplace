d1c817590413a82ccb91ef5f5899b98d5db1f572
Sudhindra Pai <144920228+sudhin05@users.noreply.github.com>
2023-12-24 11:19:10 +0530

    Update NFTMarketplaceConnect.js

--- connect/NFTMarketplaceConnect.js
+++ connect/NFTMarketplaceConnect.js
@@ -1,4 +1,20 @@
-import web3Modal from "web3modal";
-import { ethers } from "ether";
-import {NFTMarketplaceAddress, NFTMarketplaceABI} from "./constents"
+import React, { useState, useEffect, useContext} from 'react'
+//import ether import web3Modal
 
+import web3Modal from "web3modal";
+import {ethers} from "ethers";
+import Router from "next/router";
+
+//INTERNAL IMPORT
+import { NFTMarketplaceAddress, NFTMarketplaceABI } from './constents';
+
+export const NFTMarketplaceContext = React.createContext();
+
+export const NFTMarketplaceProvider = ({ children }) => {
+    const titleData = "Explore digital art & NFTs";
+    return(
+        <NFTMarketplaceContext.Provider value={{titleData}}>
+            {children}
+        </NFTMarketplaceContext.Provider>
+    );
+};
