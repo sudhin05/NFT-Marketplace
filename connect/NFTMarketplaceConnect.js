fb71e1053068f195ad2335bec91a00a547dd24b9
Sudhindra Pai <144920228+sudhin05@users.noreply.github.com>
2023-12-24 23:43:12 +0530

    Update NFTMarketplaceConnect.js

--- connect/NFTMarketplaceConnect.js
+++ connect/NFTMarketplaceConnect.js
@@ -52,7 +52,7 @@ export const NFTMarketplaceProvider = ({ children }) => {
               method: "eth_requestAccounts",
             });
             setWalletAddress(accounts[0]);
-            window.location.reload();
+            //window.location.reload();
 
           } catch (error) {
             console.log('Error connecting...');
@@ -110,11 +110,149 @@ export const NFTMarketplaceProvider = ({ children }) => {
     //--CREATE NFT FUNCTION
     const createNFT = async (name, price, image, description, router) => {
         if (!name || !description || !price || !image)
-        return setError("Data is Missing"), setOpenError(true);
-      
-        
+            return setError("Data is Missing"), setOpenError(true);
 
-      } 
+        const data =JSON.stringify({ name, description, image});
+
+        try {
+            const response = await axios({
+                method: "POST",
+                url: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+                data: data,
+                headers: {
+                    pinata_api_key: `9d03a2850c7a2c190538`,
+                    pinata_secret_api_key: `
+                    19df2cf3af00256fc86448cee4cd5796e58abc0dcd62ba86090c28aaad4efba5`,
+                    "Content-Type": "application/json",
+
+                },
+            });
+
+            const url = `https://gateway.pinata.cloud/ipfs/${response.data.IpfsHash}`;
+            console.log(url);
+
+            await createSale(url, price);
+            router.push("/searchPage");
+        }   catch (error) {
+            setError("Error while creating NFT");
+            setOpenError(true);
+        }
+      }; 
+
+    //-CREATE SALE FUNCTION
+    const createSale = async(url, formInputPrice, isReselling, id) => {
+        try {
+            const price = ethers.utils.parseUnits(formInputPrice,"ether");
+            const contract = await connectingWithSmartContract();
+            
+            const listingPrice = await contract.getListingPrice();
+            const transaction = !isReselling ? await contract.createToken(url, price, {
+                value: listingPrice.toString()
+            }) : await contract.reSellToken(url , price,{
+                value: listingPrice.toString(),
+            });     
+            await transaction.wait();
+        }   catch (error) {
+            console.log("Error in Sale Creation");
+        }
+    };
+
+    //--FETCHNFTS FUNCTION
+
+    const fetchNFTs = async () => {
+        try {
+            const provider = new ethers.providers.JsonRpcProvider();
+            const contract = fetchContract(provider);
+
+            const data = await contract.fetchMarketItem();
+            //console.log(data)
+
+            const items = await Promise.all(
+                data.map(
+                    async({tokenId, seller, owner, price: unformattedPrice})=>{
+                    const tokenURI = await contract.tokenURI(tokenId);
+                    const {
+                        data: {image, name, description},
+                    } = await axios.get(tokenURI);
+                    const price = ethers.utils.formatUnits(
+                        unformattedPrice.toString(),
+                        "ether"
+                    );
+                    return {
+                        price,
+                        tokenId: tokenId.toNumber(),
+                        seller,
+                        owner,
+                        image,
+                        name,
+                        description,
+                        tokenURI,
+                    };
+                }
+              )
+            );
+            return items;
+
+        } catch (error) {
+            console.log("Error in NFTS Fetch");
+            
+        }
+    }; 
+
+    //--FETCHING MYNFT
+    const fetchMyNFT = async(type) => {
+        try {
+            const contract = await connectingWithSmartContract();
+            const data = 
+            type == "fetchItemsListed" 
+            ? await contract.fetchItemsListed()
+            : await contract.fetchMyNFT();
+
+            const items = await Promise.all(
+                data.map(async({tokenId, seller, owner, price: unformattedPrice})=> {
+                    const tokenURI = await contract.tokenURI(tokenId);
+                    const {
+                        data: {image, name, description},
+                    } = await axios.get(tokenURI);
+                    const price = ethers.utils.formatUnits(
+                        unformattedPrice.toString(),
+                        "ether"
+                    );
+
+                    return {
+                        price,
+                        tokenId: tokenId.toNumber(),
+                        seller,
+                        owner,
+                        image,
+                        name,
+                        description,
+                        tokenURI,
+                    };
+                }
+              )
+            );
+            return items;          
+        } catch (error) {
+            console.log("Error in Listed NFTs Fetch");
+            
+        }
+    };
+
+    //--BUY NFTS FUNCTION
+    const buyNFT = async (nft)=> {
+        try {
+            const contract = await connectingWithSmartContract();
+            const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+
+            const transaction = await contract.crea;
+
+            
+        } catch (error) {
+            console.log("Error in NFT Purchase")
+        }
+    }
+
 
 
 
@@ -123,9 +261,11 @@ export const NFTMarketplaceProvider = ({ children }) => {
 //        const contract = await connectingWithSmartContract();
 //        console.log(contract);
 //    };
+
     return(
-        <NFTMarketplaceContext.Provider value={{requestAccount,uploadtoPinata,titleData}}>
+        <NFTMarketplaceContext.Provider value={{requestAccount,uploadtoPinata,createNFT,fetchNFTs,fetchMyNFT,titleData}}>
             {children}
         </NFTMarketplaceContext.Provider>
     );
-}; 
+};
+
